0477750a6a1cf3ee5f073bdad486edef6c479d88
SakanaIsReal <skyza.kchongklahan@gmail.com>
2025-03-23 18:17:13 +0700

    feat: add the camera to the 'scan screen'

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,9 +1,10 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:trashscan/widgets/camera_nav_bar.dart';
 import '../widgets/custom_app_bar.dart';
 // import '../widgets/bottom_nav_bar.dart';
-// import '../widgets/camera_nav_bar.dart';
+// import '../widgets/background_camera.dart';
 import '../widgets/custom_back_button.dart';
 
 class ScanScreen extends StatefulWidget {
@@ -14,58 +15,106 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+  List<CameraDescription> cameras = [];
+  CameraController? cameraController;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupCameraController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: CustomAppBar(),
-      body:Container(
-  width: double.infinity, // Ensure full width
-  child: Column(
-    children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Stack(
         children: [
-          CustomBackButton(),
+          _buildCamera(),
+          Container(
+            width: double.infinity, // Ensure full width
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CustomBackButton(),
+                  ],
+                ),
+                Expanded(
+                  // Takes up all available space
+                  child: Column(
+                    mainAxisAlignment:
+                        MainAxisAlignment.center, // Center vertically
+                    crossAxisAlignment:
+                        CrossAxisAlignment.center, // Center horizontally
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromARGB(66, 134, 134, 134),
+                              blurRadius: 10,
+                              spreadRadius: 1,
+                              offset: Offset(0, 1),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 20),
+                          child: Text(
+                            "Capture your Trash!",
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40), // Space between widgets
+                      SvgPicture.asset('assets/icons/capture_mark.svg'),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 88),
+              ],
+            ),
+          ),
         ],
       ),
-      Expanded( // Takes up all available space
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-          crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                color: Colors.white,
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromARGB(66, 134, 134, 134),
-                    blurRadius: 10,
-                    spreadRadius: 1,
-                    offset: Offset(0, 1),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: Text(
-                  "Capture your Trash!",
-                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            SizedBox(height: 40), // Space between widgets
-            SvgPicture.asset('assets/icons/capture_mark.svg'),
-          ],
-        ),
-      ),
-    ],
-  ),
-),
-
       bottomNavigationBar: CameraNavBar(),
     );
+  }
+
+  Widget _buildCamera() {
+    if (cameraController == null ||
+        cameraController?.value.isInitialized == false) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return SafeArea(
+      child: SizedBox.expand(
+        child: CameraPreview(
+          cameraController!,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _setupCameraController() async {
+    List<CameraDescription> _cameras = await availableCameras();
+    if (_cameras.isNotEmpty) {
+      setState(() {
+        cameras = _cameras;
+        cameraController =
+            CameraController(_cameras.first, ResolutionPreset.high);
+      });
+      cameraController?.initialize().then((_) {
+        setState(() {});
+      });
+    }
   }
 }
