adfcc8f026beb360fa6f73680a56b9091b41818c
SakanaIsReal <skyza.kchongklahan@gmail.com>
2025-04-01 22:48:17 +0700

    feat: add tags to the trash information sheet

--- lib/widgets/info_sheet.dart
+++ lib/widgets/info_sheet.dart
@@ -57,78 +57,114 @@ class InfoSheet extends StatelessWidget {
   }
 
   Column _infomationDisplay(int id) {
-    TrashInformationModel? trashCategory = TrashInformationModel.getCategoryById(id);
+    TrashInformationModel? trashCategory =
+        TrashInformationModel.getCategoryById(id);
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("This is", style: TextStyle(fontSize: 22)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                trashCategory!.name,
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.w900),
-              ),
-              PcsTotalBadge(count: trashCategory.total, color: Colors.black)
-            ],
-          ),
-          const SizedBox(height: 8),
-          Image.asset(trashCategory.imagePath),
-          const SizedBox(height: 8),
-          trashCategory.description
-        ],
-      );
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("This is", style: TextStyle(fontSize: 22)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              trashCategory!.name,
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.w900),
+            ),
+            PcsTotalBadge(count: trashCategory.total, color: Colors.black)
+          ],
+        ),
+        _buildTags(trashCategory.tags, trashCategory.color),
+        const SizedBox(height: 8),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(24.0),
+          child: Image.asset(trashCategory.imagePath),
+        ),
+        const SizedBox(height: 8),
+        trashCategory.description
+      ],
+    );
   }
 
   Column _errorDisplay() {
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("Sorry But...", style: TextStyle(fontSize: 22)),
-          const SizedBox(height: 4),
-          const Text(
-            "Unrecognized Item",
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
-          Image.asset('assets/images/trash_not_found.png'),
-          const SizedBox(height: 8),
-          RichText(
-            text: TextSpan(
-              style: TextStyle(color: Colors.black, fontSize: 16),
-              children: [
-                TextSpan(
-                  text: "The item you scanned could not be recognized.\n\n",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                TextSpan(
-                  text: "Try the following steps:\n\n",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                WidgetSpan(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        _buildBulletPoint(
-                            "Check Lighting & Angle: Ensure the item is well-lit and clearly visible in the camera."),
-                        _buildBulletPoint(
-                            "Try Again: Adjust the position and rescan."),
-                        _buildBulletPoint(
-                            "Manually Select: If scanning fails, you can manually search for the item in the list."),
-                        _buildBulletPoint(
-                            "Report Issue: If the item is missing from our database, consider reporting it so we can improve recognition."),
-                      ],
-                    ),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Sorry But...", style: TextStyle(fontSize: 22)),
+        const SizedBox(height: 4),
+        const Text(
+          "Unrecognized Item",
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8),
+        Image.asset('assets/images/trash_not_found.png'),
+        const SizedBox(height: 8),
+        RichText(
+          text: TextSpan(
+            style: TextStyle(color: Colors.black, fontSize: 16),
+            children: [
+              TextSpan(
+                text: "The item you scanned could not be recognized.\n\n",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              TextSpan(
+                text: "Try the following steps:\n\n",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              WidgetSpan(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildBulletPoint(
+                          "Check Lighting & Angle: Ensure the item is well-lit and clearly visible in the camera."),
+                      _buildBulletPoint(
+                          "Try Again: Adjust the position and rescan."),
+                      _buildBulletPoint(
+                          "Manually Select: If scanning fails, you can manually search for the item in the list."),
+                      _buildBulletPoint(
+                          "Report Issue: If the item is missing from our database, consider reporting it so we can improve recognition."),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          )
-        ],
-      );
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
+}
+
+SizedBox _buildTags(List<String> tags, Color color) {
+  return SizedBox(
+    height: 40, // Fixed height to prevent overflow
+    child: ListView.separated(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(), // iOS-style scrolling
+      // padding: const EdgeInsets.symmetric(horizontal: 12), // Outer padding
+      itemCount: tags.length,
+      separatorBuilder: (context, index) => const SizedBox(width: 8),
+      itemBuilder: (context, index) => Chip(
+        side: BorderSide.none,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        label: Text(
+          tags[index],
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 10), // Inner chip padding
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, // Compact touch target
+      ),
+    ),
+  );
 }
 
 Widget _buildBulletPoint(String text) {
