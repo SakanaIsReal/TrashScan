5d103259949055e9b4a44301cd69d434216907c8
SakanaIsReal <skyza.kchongklahan@gmail.com>
2025-04-02 12:33:09 +0700

    fix: change hard coded card to ListView.builder

--- lib/screens/trashDict_screen.dart
+++ lib/screens/trashDict_screen.dart
@@ -1,175 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:trashscan/widgets/custom_app_bar.dart';
 import 'package:trashscan/widgets/bottom_nav_bar.dart';
+import '../models/trash_information_model.dart';
 
 class TrashdictScreen extends StatelessWidget {
   const TrashdictScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final categories = TrashInformationModel.getCategories();
+
     return Scaffold(
-        appBar: CustomAppBar(),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Type of",
-                style: TextStyle(fontSize: 25),
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Type of",
+              style: TextStyle(fontSize: 20, height: 0),
+            ),
+            Text(
+              "Waste",
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, height: 0),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  final category = categories[index];
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 16),
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 204, 204, 204),
+                        width: 1.0,
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: Icon(
+                        category.icon,
+                        color: category.color,
+                        size: 26,
+                      ),
+                      title: Text(
+                        category.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey,
+                        size: 20,
+                      ),
+                      onTap: () {
+                        context.pushNamed(
+                          'education',
+                          pathParameters: {'id': category.id.toString()},
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
-              Text(
-                "Waste",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 30),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.only(bottom: 16),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ListTile(
-                  leading: Image.asset(
-                    'assets/icons/plastic_icon.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text('Plastic',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  trailing: Image.asset(
-                    'assets/icons/arrow_right.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.only(bottom: 16),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ListTile(
-                  leading: Image.asset(
-                    'assets/icons/paper.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text('Paper',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  trailing: Image.asset(
-                    'assets/icons/paper.png',
-                    width: 3,
-                    height: 3,
-                  ),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.only(bottom: 16),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ListTile(
-                  leading: Image.asset(
-                    'assets/icons/battery_0_bar_icon.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text('E-Waste',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  trailing: Image.asset(
-                    'assets/icons/arrow_right.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.only(bottom: 16),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ListTile(
-                  leading: Image.asset(
-                    'assets/icons/glass_icon.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text('Glass',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  trailing: Image.asset(
-                    'assets/icons/arrow_right.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.only(bottom: 16),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ListTile(
-                  leading: Image.asset(
-                    'assets/icons/organic_icon.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text('Organic',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  trailing: Image.asset(
-                    'assets/icons/arrow_right.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  onTap: () {},
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.only(bottom: 16),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: ListTile(
-                  leading: Image.asset(
-                    'assets/icons/hazardous_icon.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  title: Text('Hazardous Waste',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  trailing: Image.asset(
-                    'assets/icons/arrow_right.png',
-                    width: 30,
-                    height: 30,
-                  ),
-                  onTap: () {},
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-        bottomNavigationBar: BottomNavBar(selectedIndex: 2));
+      ),
+      bottomNavigationBar: BottomNavBar(selectedIndex: 2),
+    );
   }
 }
