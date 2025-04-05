356fb3500f335e80dd28e29dc2c784c4347aee87
SakanaIsReal <skyza.kchongklahan@gmail.com>
2025-04-06 05:25:38 +0700

    feat: add motivation quote in diary screen

--- lib/screens/diary_screen.dart
+++ lib/screens/diary_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/bottom_nav_bar.dart';
 import '../widgets/diary_summary.dart';
@@ -43,18 +44,24 @@ class _DiaryScreenState extends State<DiaryScreen> {
                   child: Padding(
                     padding: const EdgeInsets.only(
                         top: 14.0, bottom: 14, left: 24, right: 24),
-                    child: DiarySummary(
-                      fontSize: 16.0, // Custom font size
-                      summaryItems: [
-                        SummaryItem('Plastic', 57),
-                        SummaryItem('Metal', 9),
-                        SummaryItem('Glass', 6),
-                        SummaryItem('Paper', 13),
-                        SummaryItem('Organic Waste', 47),
-                        SummaryItem('Textiles', 2),
-                        SummaryItem('E-waste', 1),
-                        SummaryItem('Hazardous Waste', 3),
-                        SummaryItem('Miscellaneous', 23),
+                    child: Column(
+                      children: [
+                        DiarySummary(
+                          fontSize: 16.0, // Custom font size
+                          summaryItems: [
+                            SummaryItem('Plastic', 57),
+                            SummaryItem('Metal', 9),
+                            SummaryItem('Glass', 6),
+                            SummaryItem('Paper', 13),
+                            SummaryItem('Organic Waste', 47),
+                            SummaryItem('Textiles', 2),
+                            SummaryItem('E-waste', 1),
+                            SummaryItem('Hazardous Waste', 3),
+                            SummaryItem('Miscellaneous', 23),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        SvgPicture.asset('assets/icons/motivated.svg')
                       ],
                     ),
                   )),
